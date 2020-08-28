199ad3b78f8e1935b25f6aa3031e424381db1139
alxghub <al_ex105@hotmail.com>
2020-08-27 20:54:54 -0500

    Agregue el hola.cpp

---
+++ hola.cpp
@@ -0,0 +1,29 @@
+/*
+Programa en C++ que permita convertir un numero del 1 al 1000 a su equivalente a Numero Romano
+*/
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int na[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+string nr[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+void conNumRom(int n){
+	int i=0;
+	while(n>0){  //n=5
+		if(n>=na[i]){
+			cout<<nr[i];
+			n=n-na[i];
+		}
+		else
+			i=i+1;
+	}
+}
+
+int main(){
+	int n;
+	cout<<"Ingrese un numero: "; cin>>n;
+	conNumRom(n);
+    getchar();
+	return 0;
+}
